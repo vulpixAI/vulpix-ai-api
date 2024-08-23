d22990e39cc0b006243d05c731915165e499f8f0
Enzo Martins <enzo.martins@sptech.school>
2024-08-23 18:41:28 -0300

    feat: setup do projeto

---
+++ src/main/java/com/vulpix/api/VulpixAiApiApplication.java
@@ -0,0 +1,13 @@
+package com.vulpix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulpixAiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulpixAiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vulpix/api/vulpix_ai_api/VulpixAiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vulpix.api.vulpix_ai_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulpixAiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
