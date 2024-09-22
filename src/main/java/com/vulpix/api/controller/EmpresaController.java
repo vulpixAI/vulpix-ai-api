0d173ca0225c60a72e946664cbe40d89bd844663
Enzo Martins <enzo.martins@sptech.school>
2024-09-22 18:36:43 -0300

    :recycle: refactor: Refatoração Crud de Empresa

--- src/main/java/com/vulpix/api/controller/EmpresaController.java
+++ src/main/java/com/vulpix/api/controller/EmpresaController.java
@@ -1,67 +1,35 @@
 package com.vulpix.api.controller;
 
 import com.vulpix.api.entity.Empresa;
-import com.vulpix.api.entity.Integracao;
-import com.vulpix.api.repository.EmpresaRepository;
-import com.vulpix.api.repository.IntegracaoRepository;
+import com.vulpix.api.services.EmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/empresas")
 public class EmpresaController {
     @Autowired
-    private EmpresaRepository EmpresaRepository;
+    EmpresaService empresaService;
 
     @PostMapping
-    public ResponseEntity<Empresa> cadastrar(
-            @RequestBody Empresa novaEmpresa
-    ) {
-        Optional<Empresa> empresaExistente = EmpresaRepository.findByRazaoSocialAndCnpj(novaEmpresa.getRazaoSocial(), novaEmpresa.getCnpj());
-
-        if (empresaExistente.isPresent()){
+    public ResponseEntity<Empresa> cadastrar(@RequestBody Empresa novaEmpresa) {
+        if (empresaService.empresaExistePorRazaoSocialECnpj(novaEmpresa.getRazaoSocial(), novaEmpresa.getCnpj())) {
             return ResponseEntity.status(409).build();
         }
-        novaEmpresa.setId(null);
-        return ResponseEntity.status(201).body(EmpresaRepository.save(novaEmpresa));
+        Empresa empresaSalva = empresaService.salvarEmpresa(novaEmpresa);
+        return ResponseEntity.status(201).body(empresaSalva);
     }
 
-    @PatchMapping
-    public ResponseEntity<Empresa> atualizar(
-            @PathVariable UUID id,
-            @RequestBody Empresa empresaAtualizada
-    ){
-        Optional<Empresa> empresa = EmpresaRepository.findById(id);
-
-        if (empresa.isEmpty()){
+    @PatchMapping("/{id}")
+    public ResponseEntity<Empresa> atualizar(@PathVariable UUID id, @RequestBody Empresa empresaAtualizada) {
+        Empresa empresaAtualizadaSalva = empresaService.atualizarEmpresa(id, empresaAtualizada);
+        if (empresaAtualizadaSalva == null) {
             return ResponseEntity.status(404).build();
         }
-        Empresa empresaExistente = empresa.get();
-
-        if (empresaAtualizada.getCep() != null && !empresaAtualizada.getCep().isEmpty()){
-            empresaExistente.setCep(empresaAtualizada.getCep());
-        }
-        if (empresaAtualizada.getLogradouro() != null && !empresaAtualizada.getLogradouro().isEmpty()){
-            empresaExistente.setLogradouro(empresaAtualizada.getLogradouro());
-        }
-        if (empresaAtualizada.getNumero() != null && !empresaAtualizada.getNumero().isEmpty()){
-            empresaExistente.setNumero(empresaAtualizada.getNumero());
-        }
-        if (empresaAtualizada.getBairro() != null && !empresaAtualizada.getBairro().isEmpty()){
-            empresaExistente.setBairro(empresaAtualizada.getBairro());
-        }
-        if (empresaAtualizada.getEstado() != null && !empresaAtualizada.getEstado().isEmpty()){
-            empresaExistente.setEstado(empresaAtualizada.getEstado());
-        }
-        if (empresaAtualizada.getCidade() != null && !empresaAtualizada.getCidade().isEmpty()){
-            empresaExistente.setCidade(empresaAtualizada.getCidade());
-        }
-
-        EmpresaRepository.save(empresaExistente);
-        return ResponseEntity.status(200).body(empresaExistente);
+        return ResponseEntity.ok(empresaAtualizadaSalva);
     }
+
 }
---
+++ src/main/java/com/vulpix/api/services/EmpresaService.java
@@ -0,0 +1,58 @@
+package com.vulpix.api.services;
+
+import com.vulpix.api.entity.Empresa;
+import com.vulpix.api.repository.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class EmpresaService {
+    @Autowired
+     EmpresaRepository empresaRepository;
+
+    public boolean empresaExistePorRazaoSocialECnpj(String razaoSocial, String cnpj) {
+        return empresaRepository.findByRazaoSocialAndCnpj(razaoSocial, cnpj).isPresent();
+    }
+
+    public Empresa salvarEmpresa(Empresa novaEmpresa) {
+        novaEmpresa.setId(null);
+        return empresaRepository.save(novaEmpresa);
+    }
+
+    public Empresa atualizarEmpresa(UUID id, Empresa empresaAtualizada) {
+        Optional<Empresa> empresaOpt = empresaRepository.findById(id);
+
+        if (empresaOpt.isEmpty()) {
+            return null;
+        }
+
+        Empresa empresaExistente = empresaOpt.get();
+        atualizarDadosEmpresa(empresaExistente, empresaAtualizada);
+
+        return empresaRepository.save(empresaExistente);
+    }
+
+    private void atualizarDadosEmpresa(Empresa empresaExistente, Empresa empresaAtualizada) {
+        if (empresaAtualizada.getCep() != null && !empresaAtualizada.getCep().isEmpty()) {
+            empresaExistente.setCep(empresaAtualizada.getCep());
+        }
+        if (empresaAtualizada.getLogradouro() != null && !empresaAtualizada.getLogradouro().isEmpty()) {
+            empresaExistente.setLogradouro(empresaAtualizada.getLogradouro());
+        }
+        if (empresaAtualizada.getNumero() != null && !empresaAtualizada.getNumero().isEmpty()) {
+            empresaExistente.setNumero(empresaAtualizada.getNumero());
+        }
+        if (empresaAtualizada.getBairro() != null && !empresaAtualizada.getBairro().isEmpty()) {
+            empresaExistente.setBairro(empresaAtualizada.getBairro());
+        }
+        if (empresaAtualizada.getEstado() != null && !empresaAtualizada.getEstado().isEmpty()) {
+            empresaExistente.setEstado(empresaAtualizada.getEstado());
+        }
+        if (empresaAtualizada.getCidade() != null && !empresaAtualizada.getCidade().isEmpty()) {
+            empresaExistente.setCidade(empresaAtualizada.getCidade());
+        }
+    }
+}
