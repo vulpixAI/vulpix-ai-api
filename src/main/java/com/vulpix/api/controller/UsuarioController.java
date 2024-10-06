0a4bb35ccd2b08af86779c0174cf6dfd80967954
lucas-oristanio <lucas.faes@sptech.school>
2024-10-06 02:36:20 -0300

    fix: corrige retorno de status da validacao de empresa existente

--- src/main/java/com/vulpix/api/controller/UsuarioController.java
+++ src/main/java/com/vulpix/api/controller/UsuarioController.java
@@ -32,7 +32,7 @@ public class UsuarioController {
         Empresa empresaEntidade = CadastroRequisicaoMapper.criaEntidadeEmpresa(cadastroInicial, usuarioSalvo);
         Empresa empresaSalva = empresaService.salvarEmpresa(empresaEntidade);
 
-        if (empresaSalva == null) return ResponseEntity.status(401).build();
+        if (empresaSalva == null) return ResponseEntity.status(409).build();
 
         CadastroRetornoDto retorno = CadastroRequisicaoMapper.retornoCadastro(usuarioSalvo, empresaSalva);
 
