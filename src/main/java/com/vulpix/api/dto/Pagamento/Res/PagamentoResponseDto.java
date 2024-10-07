529b09c02ffbf4bf1db9defb7dd2ab1537aac333
Enzo Martins <enzo.martins@sptech.school>
2024-10-07 20:09:23 -0300

    remove: api de pagamento

--- src/main/java/com/vulpix/api/dto/Pagamento/Res/PagamentoResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.vulpix.api.dto.Pagamento.Res;
-
-import lombok.Data;
-
-@Data
-public class PagamentoResponseDto {
-    private String urlPagamento;
-}
--- src/main/java/com/vulpix/api/services/PlanoService.java
+++
@@ -1,8 +0,0 @@
-package com.vulpix.api.services;
-
-import com.vulpix.api.repository.PlanoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public interface PlanoService {
-
-}
