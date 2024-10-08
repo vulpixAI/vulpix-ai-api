040a3856271d57c29e8a6f99097c03d865fa00db
Enzo Martins <enzo.martins@sptech.school>
2024-10-08 19:40:51 -0300

    refactor: add nome app

--- src/main/java/com/vulpix/api/VulpixAiApiApplication.java
+++ src/main/java/com/vulpix/api/VulpixAiApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Aplicacao {
+public class VulpixAiApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Aplicacao.class, args);
+		SpringApplication.run(VulpixAiApiApplication.class, args);
 	}
 
 }
