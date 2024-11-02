47fd33d850fd1b60db7911e0f7e3a09f08cc3a29
Enzo Martins <enzo.martins@sptech.school>
2024-11-02 00:32:28 -0300

    refactor: swagger controller empresa

--- src/main/java/com/vulpix/api/Controller/EmpresaController.java
+++ src/main/java/com/vulpix/api/Controller/EmpresaController.java
@@ -7,6 +7,8 @@ import com.vulpix.api.dto.Empresa.FormularioRequisicaoDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,28 +23,33 @@ public class EmpresaController {
     @Autowired
     private UsuarioAutenticadoUtil usuarioAutenticadoUtil;
 
-    @Operation(summary = "Atualiza os dados da empresa",
-            description = "Atualiza os dados da empresa associada ao usuário autenticado.")
+    @Operation(summary = "Atualiza dados da empresa do usuário autenticado", description = "Atualiza os dados da empresa associada ao usuário atualmente autenticado.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Empresa atualizada com sucesso."),
-            @ApiResponse(responseCode = "404", description = "Empresa não encontrada.")
+            @ApiResponse(responseCode = "200", description = "Empresa atualizada com sucesso.",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{ \"nome\": \"Empresa Atualizada\" }"))),
+            @ApiResponse(responseCode = "404", description = "Empresa não encontrada.",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{ \"message\": \"Erro: Empresa não encontrada.\" }")))
     })
-    @PatchMapping("")
+    @PatchMapping
     public ResponseEntity<Empresa> atualizar(@RequestBody Empresa empresaAtualizada) {
         UserDetails userDetails = usuarioAutenticadoUtil.getUsuarioDetalhes();
         String emailUsuario = userDetails.getUsername();
         Empresa empresa = empresaService.buscarEmpresaPeloUsuario(emailUsuario);
-        Empresa empresaAtualizadaSalva = empresaService.atualizarEmpresa(empresa, empresaAtualizada);
-        if (empresaAtualizadaSalva == null) {
-            return ResponseEntity.status(404).build();
+
+        if (empresa == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+
+        Empresa empresaAtualizadaSalva = empresaService.atualizarEmpresa(empresa, empresaAtualizada);
         return ResponseEntity.ok(empresaAtualizadaSalva);
     }
-    @Operation(summary = "Cadastra um novo formulário",
-            description = "Cadastra um formulário associado à empresa do usuário autenticado.")
+
+    @Operation(summary = "Cadastra um novo formulário para a empresa do usuário autenticado", description = "Adiciona um novo formulário associado à empresa do usuário autenticado.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Formulário cadastrado com sucesso."),
-            @ApiResponse(responseCode = "404", description = "Empresa não encontrada.")
+            @ApiResponse(responseCode = "200", description = "Formulário cadastrado com sucesso.",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{ \"formularioId\": \"12345\", \"descricao\": \"Novo formulário cadastrado\" }"))),
+            @ApiResponse(responseCode = "404", description = "Empresa não encontrada.",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{ \"message\": \"Erro: Empresa não encontrada.\" }")))
     })
     @PostMapping("/form")
     public ResponseEntity<FormularioRequisicaoDto> cadastrarFormulario(@RequestBody FormularioRequisicaoDto formulario) {
@@ -50,16 +57,20 @@ public class EmpresaController {
         String emailUsuario = userDetails.getUsername();
         Empresa empresa = empresaService.buscarEmpresaPeloUsuario(emailUsuario);
 
-        if (empresa == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        if (empresa == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
 
         FormularioRequisicaoDto retorno = empresaService.cadastrarFormulario(empresa, formulario);
-        return ResponseEntity.ok().body(retorno);
+        return ResponseEntity.ok(retorno);
     }
-    @Operation(summary = "Busca o formulário da empresa",
-            description = "Busca o formulário associado à empresa do usuário autenticado.")
+
+    @Operation(summary = "Busca o formulário da empresa do usuário autenticado", description = "Retorna o formulário associado à empresa do usuário autenticado.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Formulário encontrado com sucesso."),
-            @ApiResponse(responseCode = "404", description = "Formulário não encontrado.")
+            @ApiResponse(responseCode = "200", description = "Formulário encontrado com sucesso.",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{ \"formularioId\": \"12345\", \"descricao\": \"Detalhes do formulário\" }"))),
+            @ApiResponse(responseCode = "404", description = "Formulário não encontrado.",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{ \"message\": \"Erro: Formulário não encontrado.\" }")))
     })
     @GetMapping("/form")
     public ResponseEntity<FormularioRequisicaoDto> buscaFormulario() {
@@ -69,20 +80,30 @@ public class EmpresaController {
 
         FormularioRequisicaoDto formularioResponse = empresaService.buscaFormulario(empresa);
 
-        if (formularioResponse == null) return ResponseEntity.notFound().build();
-        return ResponseEntity.ok().body(formularioResponse);
+        if (formularioResponse == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(formularioResponse);
     }
 
+    @Operation(summary = "Atualiza o formulário da empresa do usuário autenticado", description = "Atualiza os dados do formulário associado à empresa do usuário autenticado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Formulário atualizado com sucesso.",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{ \"formularioId\": \"12345\", \"descricao\": \"Formulário atualizado\" }"))),
+            @ApiResponse(responseCode = "404", description = "Empresa não encontrada.",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{ \"message\": \"Erro: Empresa não encontrada.\" }")))
+    })
     @PutMapping("/form")
     public ResponseEntity<FormularioRequisicaoDto> atualizaFormulario(@RequestBody FormularioRequisicaoDto formulario) {
         UserDetails userDetails = usuarioAutenticadoUtil.getUsuarioDetalhes();
         String emailUsuario = userDetails.getUsername();
         Empresa empresa = empresaService.buscarEmpresaPeloUsuario(emailUsuario);
 
-        if (empresa == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        if (empresa == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
 
         FormularioRequisicaoDto retorno = empresaService.atualizaFormulario(empresa, formulario);
-        return ResponseEntity.ok().body(retorno);
+        return ResponseEntity.ok(retorno);
     }
-
 }
