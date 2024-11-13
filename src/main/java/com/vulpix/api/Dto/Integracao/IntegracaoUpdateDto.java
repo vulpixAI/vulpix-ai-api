6583da9227ec92384d2db756c78e1cf0b8659a4d
Lucas Oristanio Faes <142420909+lucas-oristanio@users.noreply.github.com>
2024-11-12 22:05:50 -0300

    feat: validacoes em DTOs (#11)

--- src/main/java/com/vulpix/api/Dto/Integracao/IntegracaoUpdateDto.java
+++ src/main/java/com/vulpix/api/Dto/Integracao/IntegracaoUpdateDto.java
@@ -1,13 +1,18 @@
 package com.vulpix.api.Dto.Integracao;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class IntegracaoUpdateDto {
+    @NotBlank
     private String accessToken;
+    @NotBlank
     private String clientId;
+    @NotBlank
     private String clientSecret;
+    @NotBlank
     private String igUserId;
 }
