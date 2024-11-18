e2edc9b5c9ebd8d0add3ae23ebaf15aa2f3a3829
Enzo Martins <enzo.martins@sptech.school>
2024-11-18 19:50:26 -0300

    tests: implementado classe teste service-empresa

--- src/main/java/com/vulpix/api/Dto/Agent/PublicacaoGeradaRetorno.java
+++ src/main/java/com/vulpix/api/Dto/Agent/PublicacaoGeradaRetorno.java
@@ -1,10 +1,14 @@
 package com.vulpix.api.Dto.Agent;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PublicacaoGeradaRetorno {
     private String legenda;
 
