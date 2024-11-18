7008c9af28a3bdb2a47ca52a151ba9b5a2b37c0b
lucas-oristanio <lucas.faes@sptech.school>
2024-11-18 19:26:33 -0300

    adiciona controller e service para criativos

---
+++ src/main/java/com/vulpix/api/Controller/CriativoController.java
@@ -0,0 +1,41 @@
+package com.vulpix.api.Controller;
+
+import com.vulpix.api.Dto.Criativo.CriativoResponseDto;
+import com.vulpix.api.Entity.Empresa;
+import com.vulpix.api.Service.Integracoes.AgentAi.CriativosService;
+import com.vulpix.api.Service.Usuario.Autenticacao.UsuarioAutenticadoUtil;
+import com.vulpix.api.Utils.Helpers.EmpresaHelper;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/criativos")
+@Tag(name = "Controller de Criativos")
+public class CriativoController {
+    @Autowired
+    private UsuarioAutenticadoUtil usuarioAutenticadoUtil;
+    @Autowired
+    private EmpresaHelper empresaHelper;
+    @Autowired
+    private CriativosService criativosService;
+
+    @GetMapping
+    public ResponseEntity<List<CriativoResponseDto>> buscarCriativos() {
+        UserDetails userDetails = usuarioAutenticadoUtil.getUsuarioDetalhes();
+        String emailUsuario = userDetails.getUsername();
+        Empresa empresa = empresaHelper.buscarEmpresaPeloUsuario(emailUsuario);
+
+        if (empresa == null) return ResponseEntity.status(404).build();
+
+        List<CriativoResponseDto> criativos = criativosService.buscaCriativosGerados(empresa);
+        if (criativos == null) return ResponseEntity.status(404).build();
+        return ResponseEntity.status(200).body(criativos);
+    }
+}
