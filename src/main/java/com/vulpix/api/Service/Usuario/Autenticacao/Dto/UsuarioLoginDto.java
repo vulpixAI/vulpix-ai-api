cbbc342bceb9237ad6c539be0bdfa0655463b12a
Enzo Martins <enzo.martins@sptech.school>
2024-11-18 18:10:07 -0300

    tests: implementado classe teste service-usuario

--- src/main/java/com/vulpix/api/Service/Usuario/Autenticacao/Dto/UsuarioLoginDto.java
+++ src/main/java/com/vulpix/api/Service/Usuario/Autenticacao/Dto/UsuarioLoginDto.java
@@ -1,10 +1,14 @@
 package com.vulpix.api.Service.Usuario.Autenticacao.Dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioLoginDto {
     private String email;
     private String senha;
