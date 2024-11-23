5051cae914889b50a74fbbc486677c3abd619d51
Lucas Oristanio Faes <142420909+lucas-oristanio@users.noreply.github.com>
2024-11-23 03:29:02 -0300

    feat: parametro dataFim na filtragem por data e adicionando exception personalizada (#22)

---
+++ src/main/java/com/vulpix/api/Utils/Helpers/Exceptions/InvalidDateFilterException.java
@@ -0,0 +1,7 @@
+package com.vulpix.api.Utils.Helpers.Exceptions;
+
+public class InvalidDateFilterException extends RuntimeException{
+    public InvalidDateFilterException(String message) {
+        super(message);
+    }
+}
