94a05877eb4d138aa24c90cfca95fcad0a26c6a4
Lucas Oristanio Faes <142420909+lucas-oristanio@users.noreply.github.com>
2024-12-08 17:18:10 -0300

    feat: adiciona lógica para buscar dados para o gráfico de linha (#42)
    * feat: adiciona lógica para buscar dados para o gráfico de linha
    
    * fix: dto e retorno do dia exato da semana
    
    * feat: finalizando gets para kpi e graficos da dash

--- src/main/java/com/vulpix/api/Controller/DashboardController.java
+++ src/main/java/com/vulpix/api/Controller/DashboardController.java
@@ -1,6 +1,8 @@
 package com.vulpix.api.Controller;
 
 import com.vulpix.api.Dto.Criativo.CriativoRequisicaoDto;
+import com.vulpix.api.Dto.Dashboard.DashKpiDto;
+import com.vulpix.api.Dto.Dashboard.PostInsightsDto;
 import com.vulpix.api.Entity.Empresa;
 import com.vulpix.api.Entity.PostInsights;
 import com.vulpix.api.Service.DashboardService;
@@ -9,11 +11,9 @@ import com.vulpix.api.Utils.Helpers.EmpresaHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,7 +27,7 @@ public class DashboardController {
     @Autowired
     private DashboardService dashboardService;
     
-    @GetMapping("/grafico-linha")
+    @GetMapping("/grafico-ultima-metrica-post")
     public ResponseEntity<List<PostInsights>> buscaUltimaMetricaPost() {
         UserDetails userDetails = usuarioAutenticadoUtil.getUsuarioDetalhes();
         String emailUsuario = userDetails.getUsername();
@@ -36,6 +36,32 @@ public class DashboardController {
         if (empresa == null) return ResponseEntity.status(404).build();
 
         List<PostInsights> response = dashboardService.buscaMetricaUltimoPost(empresa);
+        if (response == null) return ResponseEntity.status(204).build();
+        return ResponseEntity.status(200).body(response);
+    }
+
+    @GetMapping("/grafico-metricas-por-dia")
+    public ResponseEntity<List<PostInsightsDto>> buscaMetricasPorDia(@RequestParam LocalDate data_inicio, @RequestParam LocalDate data_fim) {
+        UserDetails userDetails = usuarioAutenticadoUtil.getUsuarioDetalhes();
+        String emailUsuario = userDetails.getUsername();
+        Empresa empresa = empresaHelper.buscarEmpresaPeloUsuario(emailUsuario);
+
+        if (empresa == null) return ResponseEntity.status(404).build();
+
+        List<PostInsightsDto> response = dashboardService.buscaMetricasPorDia(empresa, data_inicio, data_fim);
+        if (response == null) return ResponseEntity.status(204).build();
+        return ResponseEntity.status(200).body(response);
+    }
+
+    @GetMapping("/kpís")
+    public ResponseEntity<DashKpiDto> buscaDadosKpi() {
+        UserDetails userDetails = usuarioAutenticadoUtil.getUsuarioDetalhes();
+        String emailUsuario = userDetails.getUsername();
+        Empresa empresa = empresaHelper.buscarEmpresaPeloUsuario(emailUsuario);
+
+        if (empresa == null) return ResponseEntity.status(404).build();
+
+        DashKpiDto response = dashboardService.buscaKpisPorPeriodo(empresa);
         if (response == null) return ResponseEntity.status(204).build();
         return ResponseEntity.status(200).body(response);
     }
