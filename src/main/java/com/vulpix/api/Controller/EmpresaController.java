924ad4b394aab4dd22d379567ca38b18614f17e9
renancasalle <renan.goncalves@sptech.school>
2024-12-08 22:04:51 -0300

    cleanup publicacao controller

--- src/main/java/com/vulpix/api/Controller/EmpresaController.java
+++ src/main/java/com/vulpix/api/Controller/EmpresaController.java
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
