a4eff7c09d87a5d8eed97631e34082d9bb5306fa
renancasalle <renan.goncalves@sptech.school>
2024-12-08 22:25:32 -0300

    feat: plano

--- src/main/java/com/vulpix/api/Repository/PlanoRepository.java
+++ src/main/java/com/vulpix/api/Repository/PlanoRepository.java
@@ -1,6 +1,6 @@
 package com.vulpix.api.Repository;
 
-
+import com.vulpix.api.Entity.Plano;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
