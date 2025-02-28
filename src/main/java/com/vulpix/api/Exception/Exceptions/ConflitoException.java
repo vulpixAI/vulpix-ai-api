52ec03d0a416456681f9f463fdf062cebeb8738a
Leonardo Santos <83714306+leOhsantos@users.noreply.github.com>
2025-02-27 21:04:38 -0300

    refactor: nome das classes (#51)

---
+++ src/main/java/com/vulpix/api/Exception/Exceptions/ConflitoException.java
@@ -0,0 +1,7 @@
+package com.vulpix.api.Exception.Exceptions;
+
+public class ConflitoException extends RuntimeException {
+    public ConflitoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/vulpix/api/Exception/Exceptions/RequisicaoInvalidaException.java
@@ -0,0 +1,7 @@
+package com.vulpix.api.Exception.Exceptions;
+
+public class RequisicaoInvalidaException extends RuntimeException {
+    public RequisicaoInvalidaException(String message) {
+        super(message);
+    }
+}
