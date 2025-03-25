18e991df98d3530acae8530fd062dd68b3f8c5d6
leOhsantos <leonardo.santos191004@gmail.com>
2025-03-25 20:08:18 -0300

    fix: correção dto de request

--- src/main/java/com/vulpix/api/dto/googleauth/GoogleAuthOtpRequest.java
+++ src/main/java/com/vulpix/api/dto/googleauth/GoogleAuthOtpRequest.java
@@ -1,11 +1,15 @@
 package com.vulpix.api.dto.googleauth;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GoogleAuthOtpRequest {
     @NotBlank
     private String otp;
